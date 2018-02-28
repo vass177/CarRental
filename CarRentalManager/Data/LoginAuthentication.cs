182d98b4fdc788039e019829157bee85c05ff99a
Adam Vass <vass17@gmail.com>
2018-02-28 12:30:09 +0100

    User authentication

---
+++ CarRentalManager/Data/LoginAuthentication.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="LoginAuthentication.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CarRentalManager.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LoginAuthentication
+    {
+        private string user;
+        private string passwordHash;
+        private List<string> userList;
+
+        public LoginAuthentication()
+        {
+            this.userList = new List<string>();
+        }
+
+        public List<string> UserList
+        {
+            get { return this.userList; }
+            set { this.userList = value; }
+        }
+
+        public bool CheckLoginCredentials(string user, string password)
+        {
+            if (this.UserExists(user))
+            {
+                string inputPassword = this.EncodePassword(password);
+                Console.WriteLine(inputPassword);
+                Console.WriteLine(this.passwordHash);
+                if (inputPassword.Equals(this.passwordHash))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool UserExists(string u)
+        {
+            this.user = u;
+
+            // it will iterate through the database, and collect all the usernames
+            if (this.userList.Contains(this.user))
+            {
+                // if user exists, we get passwordHash from the user database
+                this.passwordHash = this.EncodePassword("jelszo");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string EncodePassword(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            byte[] inArray = HashAlgorithm.Create("SHA256").ComputeHash(bytes);
+            return Convert.ToBase64String(inArray);
+        }
+    }
+}
