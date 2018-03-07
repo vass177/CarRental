2fdc6217061824c50baf0bebdb101c082da0a7b4
csfranciska <franciska.cseleszki@gmail.com>
2018-03-07 21:40:13 +0100

    Make ClientWindow MetroWindow

--- CarRentalManager/ClientWindow.xaml.cs
+++ CarRentalManager/ClientWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace CarRentalManager
     /// <summary>
     /// Interaction logic for ClientWindow.xaml
     /// </summary>
-    public partial class ClientWindow : Window
+    public partial class ClientWindow : MetroWindow
     {
         public ClientWindow()
         {
