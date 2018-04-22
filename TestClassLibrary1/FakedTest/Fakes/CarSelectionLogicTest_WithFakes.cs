4377a23ea0ebb1e05d34647be50d6a10fe48ae15
kocsist <tomkocsi@gmail.com>
2018-04-22 02:58:23 +0200

    remove unnecessary using

--- TestClassLibrary1/FakedTest/Fakes/CarSelectionLogicTest_WithFakes.cs
+++ TestClassLibrary1/FakedTest/Fakes/CarSelectionLogicTest_WithFakes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using System.Threading.Tasks;
-using Data;
+﻿using Data;
 using Data.DataHandling;
 
 namespace TestClassLibrary1.FakedTest.Fakes
