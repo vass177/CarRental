59802955b19226d5430fdea92114ce40f3773565
kocsist <tomkocsi@gmail.com>
2018-04-22 02:53:20 +0200

    Create folder FakedTest.Fakes and CarSelectionLogicTest_WithFakes

---
+++ TestClassLibrary1/FakedTest/Fakes/CarSelectionLogicTest_WithFakes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Data;
+using Data.DataHandling;
+
+namespace TestClassLibrary1.FakedTest.Fakes
+{
+    [TextFixture]
+    internal class CarSelectionLogicTest_WithFakes
+    {
+        private Car[] cars;
+        private CarDataHandler carHandler;
+
+    }
+}
