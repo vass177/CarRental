b6878b8400d587297fbea07fff9ec1c648933af0
kocsist <tomkocsi@gmail.com>
2018-04-24 05:58:17 +0200

    Create CarSelectionLogicTest_WithFakes in Tester branch

---
+++ Test_BL/FakedTest/Fakes/CarSelectionLogicTest_WithFakes.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic;
+using Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_BL.FakedTests.Fakes
+{
+    [TestFixture]
+    internal class CarSelectionLogicTest_WithFakes
+    {
+        private Car[] cars;
+        private CarHandlingLogic carHandlingLogic;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+        private void CreateCarArray()
+        {
+
+        }
+    }
+}
