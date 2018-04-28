0c2ebb1a909609b2dac944d7713b32b417f5171c
kocsist <tomkocsi@gmail.com>
2018-04-28 19:10:48 +0200

    Create test class Test_IDataBase_methods

---
+++ Test_BL/FakedTest/Test_IDataBase_methods..cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_BL.FakedTest
+{
+    [TestFixture]
+    class Test_IDataBase_methods
+    {
+        [OneTimeSetUp]
+
+    }
+}
