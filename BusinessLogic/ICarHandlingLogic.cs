43c993d192276fce2d3cc88ea1240ca492ce1fb3
kocsist <tomkocsi@gmail.com>
2018-05-01 10:37:37 +0200

    create interface

---
+++ BusinessLogic/ICarHandlingLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Interface for basic data handling operations
+    /// </summary>
+    interface ICarHandlingLogic
+    {
+
+    }
+}
