9cecc58c190d339e5b0a61a58f80319d0e11a2c8
kocsist <tomkocsi@gmail.com>
2018-05-05 04:42:36 +0200

    create class FakeCarHandlingLogic

---
+++ Test_BL/FakedTest/Fakes/FakeCarHandlingLogic.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Test_BL.FakedTest.Fakes
+{
+    /// <summary>
+    /// ICarHandlingogic implementor for testing purposes
+    /// </summary>
+    public class FakeCarHandlingLogic : ICarHandlingLogic
+    {
+        public IList<Car> Cars { get; set; }
+
+        public IList<Rental> Rental { get; set; }
+
+        public void AddNewCar(Car newCar, string photoPath)
+        {
+           
+        }
+
+        public void DeleteCar(Car selectedCar)
+        {
+            
+        }
+
+        public void DeleteCarOrders(IQueryable<Rental> rentals)
+        {
+            
+        }
+
+        public IList<Car> GetAllCarList()
+        {
+            
+        }
+
+        public void UpdateCar()
+        {
+            
+        }
+    }
+}
