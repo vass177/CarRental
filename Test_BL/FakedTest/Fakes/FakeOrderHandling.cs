ffbb50b7050e3d5d7cb3bdea93d01ed9fdeba827
kocsist <tomkocsi@gmail.com>
2018-05-05 10:48:36 +0200

    created class FakeOrderHandling an IOrderHandling implementor

---
+++ Test_BL/FakedTest/Fakes/FakeOrderHandling.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Test_BL.FakedTest.Fakes
+{
+    public class FakeOrderHandling : IOrderHandling
+    {
+        public IList<Rental> GetAllRentalList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Service> GetAllServiceList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> HowMuchCarID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumberOfRental(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumberOfServices(Service service)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> OrderRevenue(bool onlyForClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
