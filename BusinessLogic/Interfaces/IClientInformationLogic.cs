f72319ed78b005bf4d3fc8dfb6e0a36baff65426
kocsist <tomkocsi@gmail.com>
2018-05-05 02:16:11 +0200

    created interface ClientInformationLogic

--- BusinessLogic/Interfaces/IClientInformationLogic.cs
+++ BusinessLogic/Interfaces/IClientInformationLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace BusinessLogic.Interfaces
 {
     public interface IClientInformationLogic
     {
-    }
+        IList<Client> GetAllClientList();
+
+        void DeleteClient(Client selectedClient);
+
+        void DeleteClientOrders(IQueryable<Rental> rentals);
+
+        void UpdateClient();
+
+        Client GetLoggedInClient(string userName);
+     }
 }
