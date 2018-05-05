78fb008ae13405d45c3ac88bb6d0e955ce74b685
kocsist <tomkocsi@gmail.com>
2018-05-05 11:41:27 +0200

    created class TestIXXXLogicFakes

---
+++ Test_BL/FakedTest/TestIXXXLogicFakes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_BL.FakedTest
+{
+    /// <summary>
+    /// Test class for IXXXLogic implementor fakes 
+    /// </summary>
+    class TestIXXXLogicFakes
+    {
+    }
+}
